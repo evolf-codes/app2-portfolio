ff1dc23734afa01ab1b668dacfc0657f0d61df3f
Eric Volfson <saucers.mikado_0a@icloud.com>
2024-11-15 21:58:10 -0500

    update email

--- send_email.py
+++ send_email.py
@@ -1,18 +1,18 @@
 import os
 import smtplib
 import ssl
-
+import streamlit as st
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = "465"
 
     username = "eric.codes.qa@gmail.com"
-    password = os.getenv("G_PASS")
+    email_password = st.secrets["EMAIL_PASSWORD"]
 
     my_context = ssl.create_default_context()
     receiver = "eric.codes.qa@gmail.com"
 
     with smtplib.SMTP_SSL(host, port, context=my_context) as server:
-        server.login(username, password)
+        server.login(username, email_password)
         server.sendmail(username, receiver, message)
